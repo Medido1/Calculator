29b7897827173f79065e4d20dd9ddbe0908f7901
Medido1 <medi1222@gmail.com>
2023-02-11 20:41:56 +0100

    add functions to deal with operator button. do arithmetic operations and show result

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ const calculator = {
 }
 
 function inputDigit(digit){
-  if(calculator.displayValue === "0"){
+  if (calculator.waitingForSecondOperand){
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
+  }
+  else if(calculator.displayValue === "0"){
     calculator.displayValue = digit;
   }
   else {
@@ -14,11 +18,47 @@ function inputDigit(digit){
   }
 }
 
+function handleOperator(nextOperator){
+  const {displayValue, firstOperand, operator} = calculator;
+  const input = parseFloat(displayValue);
+  if (calculator.firstOperand === null && !isNaN(input)){
+    calculator.firstOperand = input; 
+  }
+  else if (operator){
+    const result = operate(firstOperand, input, operator);
+    if (isNaN(result)){
+      calculator.displayValue = result;
+      calculator.firstOperand = null;
+      calculator.waitingForSecondOperand = false;
+      return;
+    }
+    else {
+      calculator.displayValue = Math.round(result * 10000) / 10000;
+      calculator.firstOperand = Math.round(result * 10000) / 10000;
+    }
+  }
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+}
+
+function operate(firstOperand, secondOperand, operator){
+  switch(operator){
+    case "+":
+      return firstOperand + secondOperand;
+    case "-":
+      return firstOperand - secondOperand;
+    case "*":
+      return firstOperand * secondOperand;
+    case "/":
+      if (secondOperand === 0) return "LOL"
+      return firstOperand / secondOperand;
+    default:
+      return secondOperand;
+  }
+}
+
 function updateDisplay(){
   const result = document.getElementById("result");
-  if (result.textContent.length > 12){
-    return;
-  }
   result.textContent = calculator.displayValue;
 }
 
@@ -33,7 +73,11 @@ function handleBackSpace(){
 }
 
 function handleDecimal(){
-  if (calculator.displayValue === "0"){
+  if (calculator.waitingForSecondOperand){
+    calculator.displayValue = "0.";
+    calculator.waitingForSecondOperand = false;
+  }
+  else if (calculator.displayValue === "0"){
     calculator.displayValue = "0.";
   }
   else if (!calculator.displayValue.includes(".")){
@@ -53,6 +97,12 @@ updateDisplay();
 const buttons = document.querySelectorAll("td");
 buttons.forEach(button => button.addEventListener("click", (e)=>{
   const key = e.target;
+
+  if(key.classList.contains("operator")){
+    handleOperator(key.textContent);
+    updateDisplay();
+    return;
+  }
   
   if(key.classList.contains("backspace")){
    handleBackSpace();
